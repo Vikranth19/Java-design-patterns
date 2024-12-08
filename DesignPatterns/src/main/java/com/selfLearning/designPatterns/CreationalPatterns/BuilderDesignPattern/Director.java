91f1b44315ee776d1975e3bc8db6d12b74fa59d6
Sai Charan Vikranth Paluri <vikranthpaluri@gmail.com>
2024-12-08 19:39:40 +0530

    Builder pattern

---
+++ DesignPatterns/src/main/java/com/selfLearning/designPatterns/CreationalPatterns/BuilderDesignPattern/Director.java
@@ -0,0 +1,43 @@
+package com.selfLearning.designPatterns.CreationalPatterns.BuilderDesignPattern;
+
+public class Director {
+
+    StudentBuilder studentBuilder;
+
+    Director(StudentBuilder studentBuilder){
+        this.studentBuilder = studentBuilder;
+    }
+
+    public Student createStudent(){
+
+        if(studentBuilder instanceof EngineeringStudentBuilder){
+            return createEngineeringStudent();
+        }
+        else if(studentBuilder instanceof MBAStudentBuilder){
+            return createMBAStudent();
+        }
+        return null;
+    }
+
+
+    private Student createEngineeringStudent(){
+
+        return studentBuilder.setRollNumber(1)
+                .setAge(22)
+                .setName("sj")
+                .setSubjects()
+                .setMaxCgpa(10)
+                .build();
+    }
+
+    private Student createMBAStudent(){
+
+        return studentBuilder.setRollNumber(2)
+                .setAge(24)
+                .setName("sj")
+                .setFatherName("MyFatherName")
+                .setMotherName("MyMotherName")
+                .setSubjects().build();
+
+    }
+}
---
+++ DesignPatterns/src/main/java/com/selfLearning/designPatterns/CreationalPatterns/BuilderDesignPattern/StudentBuilder.java
@@ -0,0 +1,51 @@
+package com.selfLearning.designPatterns.CreationalPatterns.BuilderDesignPattern;
+
+import java.util.List;
+
+public abstract class StudentBuilder {
+
+    int rollNumber;
+    int age;
+    int maxCgpa;
+    String name;
+    String fatherName;
+    String motherName;
+    List<String> subjects;
+
+    public StudentBuilder setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+        return this;
+    }
+
+    public StudentBuilder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public StudentBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public StudentBuilder setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+        return this;
+    }
+
+    public StudentBuilder setMotherName(String motherName) {
+        this.motherName = motherName;
+        return this;
+    }
+
+    abstract public StudentBuilder setSubjects();
+
+    public StudentBuilder setMaxCgpa(int cgpa){
+        this.maxCgpa = cgpa;
+        return this;
+    }
+
+    public Student build() {
+        return new Student(this);
+    }
+
+}
